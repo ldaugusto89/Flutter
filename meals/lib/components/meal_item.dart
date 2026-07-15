8c3957836bcabec95e36e89e432203ec6b279712
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-15 08:58:50 -0300

    Criação do componente para exibição dos items das categorias

---
+++ meals/lib/components/meal_item.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/utils/app_routes.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem(this.meal, {super.key});
+
+  final Meal meal;
+
+  void _selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(AppRoutes.mealDetails, arguments: meal);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectMeal(context),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: .circular(15)),
+        elevation: 4,
+        margin: const .all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: .only(
+                    topLeft: .circular(15),
+                    topRight: .circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: .cover,
+                  ),
+                ),
+                Positioned(
+                  right: 10,
+                  bottom: 20,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: .symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: .fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: .all(20),
+              child: Row(
+                mainAxisAlignment: .spaceAround,
+                children: [
+                Row(
+                  children: [
+                    Icon(Icons.schedule),
+                    SizedBox(width: 6,),
+                    Text('${meal.duration} min')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.work),
+                    SizedBox(width: 6,),
+                    Text(meal.complexityText)
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.attach_money),
+                    SizedBox(width: 6,),
+                    Text(meal.costText)
+                  ],
+                )
+              ],
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
