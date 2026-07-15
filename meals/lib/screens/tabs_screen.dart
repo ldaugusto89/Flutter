5cb4f2156e675289a304c3b8745b8f172ee554a6
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-15 15:26:31 -0300

    Transformado componente tabs_screen em stateful, alterado tabbar para a parte inferior do app

--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -2,26 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String,Object>> _screens = [
+    {'title': 'Categorias', 'screen':CategoriesScreen()},
+    {'title': 'Favoritos', 'screen': FavoriteScreen()}
+    ];
+
+  void _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('vamos cozinhar'),
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.category, color: Colors.white,), text: 'Categorias'),
-              Tab(icon: Icon(Icons.star, color: Colors.white,), text: 'Favoritos'),
-            ],
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey[300],
-          ),
+        appBar: AppBar(title: Text(_screens[_selectedScreenIndex]['title'] as String)),
+        body: _screens[_selectedScreenIndex]['screen'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          unselectedItemColor: Colors.grey[300],
+          currentIndex: _selectedScreenIndex,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritos'),
+          ],
         ),
-        body: TabBarView(children: [CategoriesScreen(), FavoriteScreen()],),
       ),
     );
   }
