c921fc7cfe3acfb39034278bf384a00851edecbc
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-15 10:04:27 -0300

    Criação inicial da TabScreen

---
+++ meals/lib/screens/tabs_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/favorite_screen.dart';
+
+class TabsScreen extends StatelessWidget {
+  const TabsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('vamos cozinhar'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.category), text: 'Categorias'),
+              Tab(icon: Icon(Icons.star), text: 'Favoritos'),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [CategoriesScreen(), FavoriteScreen()]),
+      ),
+    );
+  }
+}
