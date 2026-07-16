2445a2145e871e11ff9934f7301b7ef8d19a93bb
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-16 10:49:43 -0300

    Adicionado opção para adição de refeiçoes favoritas

--- meals/lib/screens/favorite_screen.dart
+++ meals/lib/screens/favorite_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
+  const FavoriteScreen(this.favoriteMeals,{super.key});
+
+  final List<Meal> favoriteMeals;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Refeições Favoritas'),
-    );
+    if(favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('Refeições Favoritas'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount:  favoriteMeals.length,
+        itemBuilder: (context,index) { 
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
   }
 }
--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/main_drawer.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({super.key});
+  const TabsScreen(this.favoritesMeals, {super.key});
+
+  final List<Meal> favoritesMeals;
 
   @override
   State<TabsScreen> createState() => _TabsScreenState();
@@ -12,22 +15,31 @@ class TabsScreen extends StatefulWidget {
 
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedScreenIndex = 0;
-  final List<Map<String,Object>> _screens = [
-    {'title': 'Categorias', 'screen':CategoriesScreen()},
-    {'title': 'Favoritos', 'screen': FavoriteScreen()}
+  late List<Map<String, Object>> _screens;
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      {'title': 'Categorias', 'screen': CategoriesScreen()},
+      {'title': 'Favoritos', 'screen': FavoriteScreen(widget.favoritesMeals)},
     ];
+  }
 
   void _selectScreen(int index) {
     setState(() {
       _selectedScreenIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(title: Text(_screens[_selectedScreenIndex]['title'] as String)),
+        appBar: AppBar(
+          title: Text(_screens[_selectedScreenIndex]['title'] as String),
+        ),
         drawer: MainDrawer(),
         body: _screens[_selectedScreenIndex]['screen'] as Widget,
         bottomNavigationBar: BottomNavigationBar(
