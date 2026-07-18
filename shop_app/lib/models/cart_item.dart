a0b57341e3e0f5850c9fae537a3eb7571e6154bf
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-18 10:39:27 -0300

    Classe modelo de itens do carrinho

---
+++ shop_app/lib/models/cart_item.dart
@@ -0,0 +1,15 @@
+class CartItem {
+  final String id;
+  final String productId;
+  final String productName;
+  final int quatity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.productId,
+    required this.productName,
+    required this.quatity,
+    required this.price,
+  });
+}
