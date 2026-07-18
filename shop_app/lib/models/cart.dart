392acac2628cb2dcabcd72a187fabdc10f32270a
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-18 10:39:14 -0300

    Classe modelo de carinho de compras

---
+++ shop_app/lib/models/cart.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/cart_item.dart';
+import 'package:shop_app/models/product.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  double get totalAmout {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quatity;
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (existingItem) => CartItem(
+          id: existingItem.id,
+          productId: existingItem.productId,
+          productName: existingItem.productName,
+          quatity: existingItem.quatity + 1,
+          price: existingItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(product.id, () => CartItem(
+        id: Random().nextDouble().toString(),
+        productId: product.id,
+        productName: product.title,
+        quatity: 1,
+        price: product.price
+      ));
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
+}
