24e53f3e66776bfae5a9b3c62034526e184a2359
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-18 10:37:32 -0300

    Componente de grid de produtos

---
+++ shop_app/lib/components/product_grid.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/components/product_item.dart';
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/models/product_list.dart';
+
+class ProductGrid extends StatelessWidget {
+  final bool showFavoriteOnly;
+  const ProductGrid(this.showFavoriteOnly,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = showFavoriteOnly ? provider.favoriteItems : provider.items;
+
+    return GridView.builder(
+      padding: .all(10),
+      itemCount: loadedProducts.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (context, i) => ChangeNotifierProvider.value(
+        value: loadedProducts[i],
+        child: ProductItem(),
+      ),
+    );
+  }
+}
