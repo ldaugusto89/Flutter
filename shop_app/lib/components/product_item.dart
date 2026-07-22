fd975ac12a347f34c9ab2a520eef8e784830e515
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-22 11:00:15 -0300

    Componente de item de produto

--- shop_app/lib/components/product_item.dart
+++ shop_app/lib/components/product_item.dart
@@ -1,53 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/models/product.dart';
+import 'package:shop_app/models/product_list.dart';
 import 'package:shop_app/utils/app_routes.dart';
+import 'package:shop_app/utils/confim_dialog.dart';
 
 class ProductItem extends StatelessWidget {
-  const ProductItem({super.key});
+  final Product product;
+  
+  const ProductItem({required this.product, super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Provider que fornece as informações entre os widgets0, o argumento listen informa que 
-    // o widget não esta escutando do provider mais, e não recebe atualização quando a notificação de atualização é acionada
-    final product = Provider.of<Product>(context, listen: false);
-
-    return ClipRRect(
-      borderRadius: .circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          title: Text(product.title, textAlign: .center),
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            //o widget Consumer informa qual parte do componente ira receber a atualização
-            // somente o que está dentro do consumer ira receber a notificação de atualização
-            icon: Consumer<Product>(
-              builder: (ctx, product, _) {
-                return Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
-                  color: Theme.of(context).colorScheme.secondary,
-                );
+    final ProductList productList = Provider.of(context);
+    return ListTile(
+      leading: CircleAvatar(backgroundImage: NetworkImage(product.imageUrl)),
+      title: Text(product.name),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).pushNamed(AppRoutes.productForm, arguments: product);
               },
+              icon: Icon(Icons.edit),
             ),
-          ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).colorScheme.secondary,
+            IconButton(
+              onPressed: () async{
+                   if (await ShopAppDialogs.confirmDialog(
+                      context: context,
+                      title: 'Remoção Produto',
+                      content: 'Tem certeza que deseja deletar o produto?',
+                      textOK: 'Sim',
+                      textCancel: 'Não',
+                    )) {
+                      return productList.deleteProduct(product);
+                    }
+                    return;
+                },
+              icon: Icon(
+                Icons.delete,
+                color: Theme.of(context).colorScheme.error,
+              ),
             ),
-          ),
-        ),
-        child: GestureDetector(
-          child: Image.network(product.imageUrl, fit: .cover),
-          onTap: () {
-            Navigator.of(
-              context,
-            ).pushNamed(AppRoutes.productDetail, arguments: product);
-          },
+          ],
         ),
       ),
     );
