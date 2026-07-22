4160095838fca5556b1b10ea5df5009238dceb47
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-22 10:57:25 -0300

    Novas telas do app, provider para compartilhar as informações entre telas

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/models/cart.dart';
+import 'package:shop_app/models/order_list.dart';
 import 'package:shop_app/models/product_list.dart';
+import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:shop_app/screens/product_form_screen.dart';
+import 'package:shop_app/screens/product_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/utils/app_routes.dart';
 
@@ -15,8 +21,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProductList()),
+        ChangeNotifierProvider(create: (_) => Cart()),
+        ChangeNotifierProvider(create: (_) => OrderList()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Shop',
@@ -30,12 +40,19 @@ class MyApp extends StatelessWidget {
           iconButtonTheme: IconButtonThemeData(
             style: IconButton.styleFrom(
               // Usando foregroundColor apontando para a cor secundária do seu tema
-              foregroundColor: Colors.white,
+              foregroundColor: Colors.grey[800],
             ),
           ),
           textTheme: TextTheme(titleLarge: TextStyle(fontFamily: 'Lato')),
         ),
-        routes: {AppRoutes.productDetail: (context) => ProductDetailScreen()},
+        routes: {
+          AppRoutes.home: (context) => ProductsOverviewScreen(),
+          AppRoutes.productDetail: (context) => ProductDetailScreen(),
+          AppRoutes.cartDetail: (context) => CartScreen(),
+          AppRoutes.orders: (context) => OrdersScreen(),
+          AppRoutes.products: (context) => ProductScreen(),
+          AppRoutes.productForm: (context) => ProductFormScreen(),
+        },
         home: ProductsOverviewScreen(),
       ),
     );
