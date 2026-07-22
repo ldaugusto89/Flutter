31a111dcdc62d9f8da953dfee1cbd44bb8f70824
ldaugusto89 <ldaugusto89@gmail.com>
2026-07-22 11:07:12 -0300

    Criação classe modelo de Lista de Produtos

--- shop_app/lib/models/product_list.dart
+++ shop_app/lib/models/product_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop_app/data/dummy_data.dart';
 import 'package:shop_app/models/product.dart';
@@ -6,7 +8,8 @@ class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
-  List<Product> get favoriteItems => _items.where((prod) => prod.isFavorite).toList();
+  List<Product> get favoriteItems =>
+      _items.where((prod) => prod.isFavorite).toList();
 
   //referencia de filtro de forma global, filtra em todas as telas do app
   // bool _showFavoriteOnly = false;
@@ -28,8 +31,46 @@ class ProductList with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  void saveProduct(Map<String, Object> data) {
+    bool hasId = data['id'] != null;
+
+    final product = Product(
+      id: hasId ? data['id'] as String : Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
+    );
+
+    if (hasId) {
+      updateProduct(product);
+    } else {
+      addProduct(product);
+    }
+  }
+
   void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    int index = _items.indexWhere((p) => p.id == product.id);
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void deleteProduct(Product product) {
+    int index = _items.indexWhere((p) => p.id == product.id);
+    if (index >= 0) {
+      _items.removeWhere((p) => p.id == product.id);
+      notifyListeners();
+    }
+  }
+
+  int get itemsCount {
+    return _items.length;
   }
 }
